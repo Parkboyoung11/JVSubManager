994c5038929ae8743442fd5c56a83874cfad9eae
Parkboyoung11 <vuhongsonjv11@gmail.com>
2018-12-12 20:59:32 +0700

    fix movie name

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites, dependent: :destroy
+  validates :name, presence: true,
+            uniqueness: {case_sensitive: false}
+  validates :language, presence: true
 end
